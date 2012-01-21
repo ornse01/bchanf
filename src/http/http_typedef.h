c4221882ca3234a546e357e737c6da9961350043
ornse01 <ornse01@users.sourceforge.jp>
2012-01-21 09:17:59 +0000

    add header file for common definition.
    git-svn-id: http://svn.sourceforge.jp/svnroot/bchan/bchanf/trunk@357 20a0b8eb-f62a-4a12-8fe1-b598822500fb

---
+++ src/http/http_typedef.h
@@ -0,0 +1,51 @@
+/*
+ * http_typedef.h
+ *
+ * Copyright (c) 2012 project bchan
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ *
+ * 3. This notice may not be removed or altered from any source
+ *    distribution.
+ *
+ */
+
+/* Vendor name: */
+/* Functionality name: http */
+/* Detail name: typedef */
+
+#include    <basic.h>
+
+#ifndef __HTTP_TYPEDEF_H__
+#define __HTTP_TYPEDEF_H__
+
+/* Functionality name: http */
+/* Detail name: */
+/* Data structure identifier: method */
+/*  rfc2616 5.1.1 Method */
+typedef enum {
+	HTTP_METHOD_OPTIONS,
+	HTTP_METHOD_GET,
+	HTTP_METHOD_HEAD,
+	HTTP_METHOD_POST,
+	HTTP_METHOD_PUT,
+	HTTP_METHOD_DELETE,
+	HTTP_METHOD_TRACE,
+	HTTP_METHOD_CONNECT,
+} HTTP_METHOD;
+
+#endif
